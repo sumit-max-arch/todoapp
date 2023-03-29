085c07c33a4cee865d527838c240fb7b002040d7
sumit711204 <sumitchowdhary01@gmail.com>
2023-03-29 14:00:09 +0530

    todo logicandreadme file info added

---
+++ README.md
@@ -0,0 +1,20 @@
+React Todo App
+A simple Todo app built with React.
+
+Features
+Add a new Todo item
+Mark a Todo item as completed and show green text
+Remove a Todo item
+
+Usage : 
+Clone the repository: 
+
+Install dependencies:
+cd TODOAPP
+npm install
+
+Run the app:
+npm start
+
+Open http://localhost:3000 to view the app in your browser.
+.
--- src/App.js
+++ src/App.js
@@ -1,8 +1,75 @@
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const handleAddItemClick = () => {
+    if (inputValue) {
+      setItems((prevItems) => [...prevItems, { text: inputValue, completed: false }]);
+      setInputValue("");
+    }
+  }
+
+  const handleItemCompletionClick = (index) => {
+    setItems((prevItems) =>
+      prevItems.map((item, i) =>
+        i === index ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
+
+  const handleItemRemovalClick = (index) => {
+    setItems((prevItems) => prevItems.filter((item, i) => i !== index));
+  }
+
   return (
-    <div className="App">
-   <h1>sumit</h1>
+    <div>
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          id="todo-input"
+          className="todo-input"
+          placeholder="Enter a todo item"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              handleAddItemClick();
+            }
+          }}
+        />
+        <button
+          id="add-item-button"
+          className="add-item-button"
+          onClick={handleAddItemClick}
+        >
+          Add Item
+        </button>
+      </div>
+      <ul id="todo-list" className="todo-list">
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={item.completed ? "completed" : ""}
+            onClick={() => handleItemCompletionClick(index)}
+            style={{ textDecoration: item.completed ? "line-through" : "none", color: item.completed ? "green" : "none" }}
+            >
+            {item.text}
+            <button
+              className="remove-item-button"
+              onClick={() => handleItemRemovalClick(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
